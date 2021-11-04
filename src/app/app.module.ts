71df42435a7c784f65ca9a1ec2c8ea7e522605d9
Goran Tatomirov <tatomirov.in12.2018@uns.ac.rs>
2021-11-04 19:14:27 +0100

    routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
 import { CardListComponent } from './components/card-list/card-list.component';
+import { GameComponent } from './components/game/game.component';
+import { MainComponent } from './components/main/main.component';
+import { RouterModule, Routes } from '@angular/router';
+import { TimerComponent } from './components/timer/timer.component';
+
+const appRoutes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'game', component: GameComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    CardListComponent
+    CardListComponent,
+    GameComponent,
+    MainComponent,
+    TimerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
