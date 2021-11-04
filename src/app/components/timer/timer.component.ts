409ea404e6b87c563366dc9db7fd2ed6cd772647
Goran Tatomirov <tatomirov.in12.2018@uns.ac.rs>
2021-11-04 22:58:00 +0100

    timer formatting

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TimerComponent implements OnInit {
 
   seconds: number = 0;
+  timeFormatted: string = '00:00';
 
   constructor() { }
 
@@ -18,7 +19,26 @@ export class TimerComponent implements OnInit {
   startTimer(){
     setInterval(() => {
       this.seconds++;
+      this.formatTime();
     },1000)
+  }
+
+  formatTime(){
+    let quotient = Math.floor(this.seconds / 60);
+    let reminder = this.seconds % 60;
+    if(quotient < 10 && reminder < 10){
+      this.timeFormatted = `0${quotient}:0${reminder}`;
+      return;
+    } else if(quotient < 10 && reminder >= 10){
+      this.timeFormatted = `0${quotient}:${reminder}`;
+      return;
+    } else if(quotient >= 10 && reminder >= 10){
+      this.timeFormatted = `${quotient}:${reminder}`;
+      return;
+    } else{
+      this.timeFormatted = `${quotient}:0${reminder}`;
+      return;
+    }
   }
 
 }
