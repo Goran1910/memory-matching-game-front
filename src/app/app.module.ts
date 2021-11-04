cdb6a9fda8b48624057b4424d44fe4486ca124dd
Goran Tatomirov <tatomirov.in12.2018@uns.ac.rs>
2021-11-04 19:20:31 +0100

    Revert " routing added"
    This reverts commit 71df42435a7c784f65ca9a1ec2c8ea7e522605d9.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,29 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
 import { CardListComponent } from './components/card-list/card-list.component';
-import { GameComponent } from './components/game/game.component';
-import { MainComponent } from './components/main/main.component';
-import { RouterModule, Routes } from '@angular/router';
-import { TimerComponent } from './components/timer/timer.component';
-
-const appRoutes: Routes = [
-  { path: '', component: MainComponent },
-  { path: 'game', component: GameComponent }
-];
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    CardListComponent,
-    GameComponent,
-    MainComponent,
-    TimerComponent
+    CardListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
